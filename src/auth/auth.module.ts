3153fed51d5d42bde5b7cca22c09165279163400
enzogit <enzo_91_10@users.noreply.github.com>
2026-01-19 09:33:52 -0300

    Merge branch 'main' of https://github.com/enzogitcode/porky_app_backend_NEST

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
-  imports:[UsersModule],
+  imports:[UsersService],
   providers: [AuthService],
   controllers: [AuthController],
   
