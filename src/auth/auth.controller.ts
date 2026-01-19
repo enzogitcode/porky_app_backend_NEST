107f35b6a012263b5447a73232a3f5bb53207427
enzogit <enzo_91_10@users.noreply.github.com>
2026-01-19 09:28:05 -0300

    authcontroller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,24 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
 
-    constructor(private readonly authService: AuthService) { }
+  /**
+   * Login de usuario
+   */
+  @Post('login')
+  async login(@Body() body: LoginDto) {
+    // 1. Validar usuario con PIN
+    const user = await this.authService.validateUser(body.name, body.pin);
 
-    @Post('register')
-    register() {
-        this.authService.register()
-
-    }
-    @Post('login')
-    login() {
-        this.authService.login()
-    }
+    // 2. Retornar JWT
+    return this.authService.login({
+      id: user._id.toString(), // user._id de Mongoose
+      name: user.name,
+      role: user.role,
+    });
+  }
 }
